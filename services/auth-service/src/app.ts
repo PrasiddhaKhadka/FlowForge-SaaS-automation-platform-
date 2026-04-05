dc8414f911488fa7126d8fb321e4d7ca671d0cfb
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-04-05 20:53:30 +0545

    Add: Simple auth server

---
+++ services/auth-service/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+
+dotenv.config()
+
+const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+app.get('/health',(req,res)=>{
+    res.json({
+    service: 'auth-service',
+    status: 'ok',
+    timestamp: new Date().toISOString(),
+  });
+})
+
+export default app;
